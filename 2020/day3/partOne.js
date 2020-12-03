aedbb48619a74a8845d58a9c6f7c6a0b1686cda8
Paunova <misspaunova@gmail.com>
2020-12-03 13:51:05 +0100

    small cleanup

--- 2020/day3/partOne.js
+++ 2020/day3/partOne.js
@@ -6,22 +6,18 @@ const partOne = (slope = {right: 3, down: 1}) => {
 
     const lines = input.split('\n');
     
-    for (let i = 0; i < lines.length; i++) {
-        let line = lines[i].trim();
-        // nothing to see here, skip line (move down)
-        if (i === 0) {
-            continue;
-        }
+    for (let i = 1; i < lines.length; i++) { // line at index 0 skipped
+        const line = lines[i].trim();
+
+        // nothing to see here, move down by skipping lines  
         if (slope.down === 2 && (i % 2 === 1)) {           
             continue;
         }
-        // move to the right
+        
+        // circular move to the right
         const toRight = position + slope.right;
-        if (toRight < line.length) {
-            position = toRight;
-        } else {
-            position = toRight - line.length;
-        }
+        position = (toRight < line.length) ? toRight : toRight - line.length;
+
         // on position, ready to check - do I sit on a tree?
         const element = line[position];
         if (element === '#') {
