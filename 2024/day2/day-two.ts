e652a200e5185f5d5cd2bfb13767e36778f30a9f
Simona Paunova <misspaunova@gmail.com>
2024-12-01 14:43:58 +0100

    add boilerplate code

---
+++ 2024/day2/day-two.ts
@@ -0,0 +1,15 @@
+import { input } from "./input";
+
+const sampleInput = ``;
+
+const calculateResult = (input: string, part: number) => {
+  let result = 0;
+
+  return result;
+};
+
+console.log(calculateResult(sampleInput, 1));
+console.log(calculateResult(sampleInput, 2));
+
+console.log(calculateResult(input, 1));
+console.log(calculateResult(input, 2));
