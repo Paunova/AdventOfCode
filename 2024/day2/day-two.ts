e203f66531ccfa8e4fd237a519039707cdc15cc2
Simona Paunova <misspaunova@gmail.com>
2024-12-02 09:47:57 +0100

    add day two solution and input

--- 2024/day2/day-two.ts
+++ 2024/day2/day-two.ts
@@ -1,15 +1,85 @@
 import { input } from "./input";
 
-const sampleInput = ``;
+const sampleInput = `7 6 4 2 1
+1 2 7 8 9
+9 7 6 2 1
+1 3 2 4 5
+8 6 4 4 1
+1 3 6 7 9`;
 
-const calculateResult = (input: string, part: number) => {
-  let result = 0;
+const differBy = (a: number, b: number) => Math.abs(a - b);
 
-  return result;
+const analyzeSingleReport = (report: string[]) => {
+  let isSafe = 0;
+
+  for (let j = 0; j < report.length - 1; j++) {
+    const currentLevel = Number(report[j]);
+    const nextLevel = Number(report[j + 1]);
+
+    if (currentLevel === nextLevel) {
+      isSafe = -1;
+    }
+
+    const differNumber = differBy(currentLevel, nextLevel);
+    const isDifferValid = differNumber > 0 && differNumber <= 3;
+
+    if (!isDifferValid) {
+      isSafe = -1;
+    }
+
+    // check if levels are in asc or desc order
+    if (currentLevel < nextLevel && isSafe === 0) {
+      isSafe = 1;
+    }
+    if (currentLevel > nextLevel && isSafe === 0) {
+      isSafe = 2;
+    }
+
+    // check if levels order is broken
+    if (currentLevel > nextLevel && isSafe === 1) {
+      isSafe = -1;
+    }
+    if (currentLevel < nextLevel && isSafe === 2) {
+      isSafe = -1;
+    }
+  }
+  return isSafe;
 };
 
-console.log(calculateResult(sampleInput, 1));
-console.log(calculateResult(sampleInput, 2));
+const analyzeSafeReports = (input: string, part: number) => {
+  let numberOfSafeReports = 0;
 
-console.log(calculateResult(input, 1));
-console.log(calculateResult(input, 2));
+  const inputByLine = input.split(/\n/);
+
+  for (let i = 0; i < inputByLine.length; i++) {
+    const report = inputByLine[i].split(" ");
+    const isSafe = analyzeSingleReport(report);
+
+    let isSafeAfterAdjustment = false;
+    // only check broken reports
+    if (part === 2 && isSafe === -1) {
+      // try all combinations with any single level removed
+      for (let k = 0; k < report.length; k++) {
+        const adjustedArr = [
+          ...report.slice(0, k),
+          ...report.slice(k + 1, report.length),
+        ];
+        const singleResult = analyzeSingleReport(adjustedArr);
+        if (singleResult === 1 || singleResult === 2) {
+          isSafeAfterAdjustment = true;
+        }
+      }
+    }
+
+    if (isSafe === 1 || isSafe === 2 || isSafeAfterAdjustment) {
+      numberOfSafeReports += 1;
+    }
+  }
+  return numberOfSafeReports;
+};
+
+console.log(analyzeSafeReports(sampleInput, 1));
+console.log(analyzeSafeReports(sampleInput, 2));
+
+console.log(analyzeSafeReports(input, 1));
+console.log(analyzeSafeReports(input, 2));
